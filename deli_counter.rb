dbc46c59941e9130fd00761a39a515264c798534
mariah <mariah@gardziola.com>
2019-06-05 19:17:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
       katz_deli.each_with_index do |new_person, index|
       line += "#{index+1}. #{new_person}"
     end 
-
+end 
 end
 
 def take_a_number(katz_deli, new_person)
