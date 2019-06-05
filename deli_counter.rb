141d891f3c5564ab6b89c6975881c76e38b25902
mariah <mariah@gardziola.com>
2019-06-05 19:11:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else katz_deli.length > 0
       puts "The line is currently:"
- 
+      each.katz_deli.with_index(1) |
     end 
 
 end
